9616ff1d26945214a8f933baa715e963be22dd0f
kento <kento@toriwatari.bird>
2021-09-20 11:41:30 +0900

    🌠 #10 graphcmsパッケージの再インストール🤔

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-typegen`,
-    `gatsby-plugin-emotion`,
+    //`gatsby-plugin-emotion`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
